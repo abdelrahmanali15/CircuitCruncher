2d14827e3e7f2ea20756a95566ba2ffb477127a9
abdelrahmanali15 <abdo.5.2001@gmail.com>
2024-07-08 18:55:06 +0300

    changing lin name to CircuitCruncher, adding scripts folder to use instead of notebook

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='CircuitCruncher',
+    version='0.1.0',
+    description='A library for circuit post analysis analysis fro ngspice and xschem',
+    author='Abdelrahman Ali',
+    author_email='abdo.5.2001@gmail.com',
+    url='https://github.com/abdelrahmanali15/CircuitCruncher/tree/notebook_extratesting',
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
